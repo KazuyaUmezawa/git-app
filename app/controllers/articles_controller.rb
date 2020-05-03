c88b5695779efcd6fe817a8235eb2deef670398f
KazuyaUmezawa <KazuyaUmezawa1024@gmail.com>
2020-05-03 19:09:24 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,25 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
+  
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+ 
   def show
   end
 
-  # GET /articles/new
+
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
+ 
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
+ 
   def create
     @article = Article.new(article_params)
 
@@ -37,8 +34,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
+ 
   def update
     respond_to do |format|
       if @article.update(article_params)
@@ -51,8 +47,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
+  
   def destroy
     @article.destroy
     respond_to do |format|
@@ -62,12 +57,12 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    
     def article_params
       params.require(:article).permit(:title, :author, :text)
     end
